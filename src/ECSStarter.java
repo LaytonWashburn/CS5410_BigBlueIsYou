61126cd219658b98b18a5bd54d8b5ad8348c478d
benjtom <152236555+benjtom@users.noreply.github.com>
2025-03-24 12:41:22 -0600

    test commit

--- src/ECSStarter.java
+++ src/ECSStarter.java
@@ -4,7 +4,7 @@ import edu.usu.graphics.Graphics2D;
 
 public class ECSStarter {
     public static void main(String[] args) {
-        try (Graphics2D graphics = new Graphics2D(1920, 1080, "Starter Project")) {
+        try (Graphics2D graphics = new Graphics2D(1920, 1080, "Big Blue is You!")) {
             graphics.initialize(Color.CORNFLOWER_BLUE);
             GameViewManager game = new GameViewManager(graphics);
             game.initialize();
