6272e0fde1394f3fef158de6b4a8364aff437278
Layton Washburn <layton.washburn1659@gmail.com>
2025-04-02 23:00:27 -0600

    Setting up systems

--- src/core/GameModel.java
+++ src/core/GameModel.java
@@ -9,7 +9,9 @@ import level.Level;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.joml.Vector2f;
 import utils.KeyBinds;
@@ -23,7 +25,7 @@ public class GameModel {
     private final List<Entity> addThese = new ArrayList<>();
     private KeyboardInput sysKeyboardInput;
     private ArrayList<ecs.Systems.System> systems;
-
+    private Map changed;
     private Graphics2D graphics;
 
     private Texture texFlag = new Texture("resources/sprites/sprites/objects/flag/flag.png");
@@ -34,6 +36,7 @@ public class GameModel {
         this.level = level;
         this.keybinds = keybinds;
         systems = new ArrayList<>();
+        this.changed = new HashMap<Long, Entity>();
     }
 
     public void initialize(Graphics2D graphics) {
@@ -45,22 +48,27 @@ public class GameModel {
         System.out.println("GameModel initialized with level: " + level);
         keybinds.printKeyBinds();
 
-        for (int i = 0; i < level.getHeight(); i++) {
-            for (int j = 0; j < level.getWidth(); j++) {
-                System.out.print(level.getGroup1()[i][j]);
-            }
-            System.out.println();
-        }
-        for (int i = 0; i < level.getHeight(); i++) {
-            for (int j = 0; j < level.getWidth(); j++) {
-                System.out.print(level.getGroup2()[i][j]);
-            }
-            System.out.println();
-        }
+        initalizeLevel(level);
+
     }
 
     public void update(double elapsedTime) {
         // Because ECS framework, input processing is now part of the update
+
+
+//        for (var system : systems) {
+//            for (var entity : system.update(elapsedTime)) {
+//                changed.put(entity.getId(), entity);
+//            }
+//        }
+//
+//        for (var entity : changed) {
+//            for (var system : systems) {
+//                system.updatedEntity(entity);
+//            }
+//        }
+
+
         sysKeyboardInput.update(elapsedTime);
 
         for (var entity : removeThese) {
@@ -86,6 +94,62 @@ public class GameModel {
     private void removeEntity(Entity entity) {
         sysKeyboardInput.remove(entity.getId());
     }
+
+    private void initalizeLevel(Level level){
+
+        for (int i = 0; i < level.getHeight(); i++) {
+            for (int j = 0; j < level.getWidth(); j++) {
+                System.out.print(level.getGroup1()[i][j]);
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < level.getHeight(); i++) {
+            for (int j = 0; j < level.getWidth(); j++) {
+                System.out.print(level.getGroup2()[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+//    private Entity createObjectTypes(Character symbol){
+//
+//        switch (symbol){
+//            case 'w':
+//                return Wall.create(0, 0);
+//            case 'r':
+//            case 'f':
+//            case 'b':
+//            case 'l':
+//            case 'g':
+//            case 'a':
+//            case 'v':
+//            case 'h':
+//            default:
+//        }
+//
+//    }
+
+//    private Entity createTextTypes(Character symbol){
+//
+//        switch (symbol){
+//            case 'W':
+//            case 'R':
+//            case 'F':
+//            case 'B':
+//            case 'L':
+//            case 'S':
+//            case 'P':
+//            case 'V':
+//            case 'A':
+//            case 'Y':
+//            case 'X':
+//            case 'N':
+//            case 'K':
+//            default:
+//        }
+//
+//    }
 
 
 }
--- src/ecs/Systems/AnimatedSprite.java
+++ src/ecs/Systems/AnimatedSprite.java
@@ -1,6 +1,16 @@
 package ecs.Systems;
 
+import ecs.Entities.Entity;
+
+import java.util.ArrayList;
+
 public class AnimatedSprite extends System{
+    ArrayList<Entity> modified;
+
+    public AnimatedSprite(){
+        super(ecs.Components.Sprite.class,
+                ecs.Components.Animated.class);
+    }
     @Override
     public void update(double elapsedTime) {
 
